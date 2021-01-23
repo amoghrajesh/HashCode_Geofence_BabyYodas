2e09d62c0da50526085b3a8a74cf13c49e9d63ae
amitk526188 <amitk526188@gmail.com>
2021-01-23 12:10:43 +0530

    added dbtest

---
+++ dbtest.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+
+db_uri = 'sqlite:///geofence.db' 
+engine = create_engine(db_uri)
+
+result1 = engine.execute('SELECT * FROM "device"')
+#print(result.fetchall())
+
+for r1 in result1:
+  print(r1)
+
+result2 = engine.execute('SELECT * FROM "geofence"')
+#print(result.fetchall())
+
+for r2 in result2:
+   print(r2)
